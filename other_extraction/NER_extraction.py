3cb625d22193d5a32350d310ffb95611eb8ffe74
Taufiq Husada Daryanto <45747001+taufiqhusada@users.noreply.github.com>
2022-02-05 06:17:11 +0700

    Update NER_extraction.py

--- other_extraction/NER_extraction.py
+++ other_extraction/NER_extraction.py
@@ -6,62 +6,61 @@ import json
 
 NAMED_ENTITY_SET = {'ORG', 'LOC', 'GEO', 'GPE'}
 
-def NER_from_df(hl1, lede, body, doc_id, nlp):
-    dict_result = {'doc_id': str(doc_id), 'ORG':[], 'LOC':[], 'GEO':[], 'GPE':[]}
-
-    if (hl1):
-        doc = nlp(str(hl1))
-        for ent in doc.ents:
-            text = str(ent.text)
-            if ((ent.label_ in NAMED_ENTITY_SET) and (text not in dict_result[ent.label_])):
-                dict_result[ent.label_].append(text)
-    if (lede):
-        doc = nlp(str(lede))
-        for ent in doc.ents:
-            text = str(ent.text)
-            if ((ent.label_ in NAMED_ENTITY_SET) and (text not in dict_result[ent.label_])):
-                dict_result[ent.label_].append(text)
-
-    if (body):
-        doc = nlp(str(body))
-        for ent in doc.ents:
-            text = str(ent.text)
-            if ((ent.label_ in NAMED_ENTITY_SET) and (text not in dict_result[ent.label_])):
-                dict_result[ent.label_].append(text)
-    
-    return dict_result
-
-
 if __name__ == "__main__":
-    data_path = '/projectnb/multilm/thdaryan/racial_bias/raw_data/data/145223'
-    nlp = spacy.load("en_core_web_trf")
+    data_path = '/projectnb/multilm/thdaryan/racial_bias/racial_bias/complete_raw_data_unique'
+    nlp = spacy.load("en_core_web_sm")
+    print(nlp.pipe_names)
 
-    list_files = []
-    with open('../regex_pipeline/list_csv_files.txt') as temp_file:
-        for filename in temp_file.readlines():
-            list_files.append(filename.strip())
+    list_files = [f'LexisNexis_BostonMedia_NewsArticles_unique_{i}.csv' for i in range(20)]
     
     for filename in list_files:
-        print(filename)
-        csv_path = os.path.join(data_path, filename)
-        df = pd.read_csv(csv_path, error_bad_lines=False,  encoding='utf8', engine='python')
+        try:
+            print(filename)
+            csv_path = os.path.join(data_path, filename)
+            df = pd.read_csv(csv_path, error_bad_lines=False,  encoding='utf8', engine='python')
 
-        inps_hl1 = df['hl1'].values
-        inps_lede = df['lede'].values
-        inps_body = df['body'].values
-        doc_ids = df['DOC-ID'].values
+            json_result = {'data':[], 'errors':[]}        
+            doc_ids = df['DOC-ID'].values
+            for id in doc_ids:
+                json_result['data'].append({'doc_id': str(id), 'ORG':[], 'LOC':[], 'GEO':[], 'GPE':[]})
 
-        json_result = {'data':[], 'errors':[]}
+            i = 0
+            batch_text_raw = df['lede'].values
+            batch_text = [str(item) for item in batch_text_raw]	
+            for doc in tqdm(nlp.pipe(batch_text, disable=["tok2vec", "tagger", "parser", "attribute_ruler", "lemmatizer"]), total=len(doc_ids)):
+                try:
+                    for ent in doc.ents:
+                        text = str(ent.text)
+                        if ((ent.label_ in NAMED_ENTITY_SET) and (text not in json_result['data'][i][ent.label_])):
+                            json_result['data'][i][ent.label_].append(text)
+                except Exception as e:
+                    with open('result_NER/errors.txt', 'w') as f:
+                        print(filename, str(e))
+                        f.write(filename, str(e))
+                i+=1
+            
+            i = 0
+            batch_text_raw = df['body'].values
+            batch_text = [str(item) for item in batch_text_raw]
+            for doc in tqdm(nlp.pipe(batch_text, disable=["tok2vec", "tagger", "parser", "attribute_ruler", "lemmatizer"]), total=len(doc_ids)):
+                try:
+                    for ent in doc.ents:
+                        text = str(ent.text)
+                        if ((ent.label_ in NAMED_ENTITY_SET) and (text not in json_result['data'][i][ent.label_])):
+                            json_result['data'][i][ent.label_].append(text)
+                except Exception as e:
+                    with open('result_NER/errors.txt', 'w') as f:
+                        print(filename, str(e))
+                        f.write(filename, str(e))
+                i+=1
+
+            output_json_file = open(f'result_NER/NER_{filename[:-4]}.json', 'w')
+            json.dump(json_result, output_json_file, indent=4)
+            output_json_file.close()
+            
+        except Exception as e:
+            with open('result_NER/errors.txt', 'w') as f:
+                print(filename, str(e))
+                f.write(filename, str(e))
+
         
-        for i in tqdm(range(len(inps_body))):
-            try:
-                result_this_id = NER_from_df(inps_hl1[i], inps_lede[i], inps_body[i], doc_ids[i], nlp)
-                json_result['data'].append(result_this_id)
-            except Exception as e: 
-                print(filename, i, e)
-                json_result['errors'].append(str(e))
-
-        output_json_file = open(f'results_NER/NER_{filename[:-4]}.json', 'w')
-        json.dump(json_result, output_json_file, indent=4)
-        output_json_file.close()
-        print('len', str(len(json_result['data'])))
