d1d0c3b2be16626697d3337c827b59ee2889e2de
Taufiq Husada Daryanto <45747001+taufiqhusada@users.noreply.github.com>
2022-03-11 19:48:02 +0700

    Create NER_extraction_sents_split.py

---
+++ other_extraction/NER_extraction_sents_split.py
@@ -0,0 +1,49 @@
+import os
+import pandas as pd
+import spacy
+from tqdm import tqdm 
+import json
+
+NAMED_ENTITY_SET = {'ORG', 'LOC', 'GEO', 'GPE'}
+
+if __name__ == "__main__":
+    nlp = spacy.load("en_core_web_sm")
+    file_split = sys.argv[1] 
+    filename = f'/projectnb/multilm/thdaryan/racial_bias/names_extraction_splitted/names_extraction_{file_split}.csv'
+    
+    with open(f'result_alligned_NER/NER_{file_split}.json', mode='w+', encoding='utf-8') as f:
+        json.dump([], f)
+    
+    for df_chunk in tqdm(pd.read_csv(filename, chunksize=10**4, header=None,  error_bad_lines=False,  encoding='utf8', engine='python')):
+        try:
+            df_chunk.rename(columns={0: 'id_from_name_extraction', 1:'sent', 2:'names', 3:'start', 4:'end', 5:'source_type', 6:'DOC-ID'}, inplace=True)
+            list_json_result = []
+            doc_ids = df_chunk['DOC-ID'].values
+            for id in doc_ids:
+                list_json_result.append({'doc_id': str(id), 'ORG':[], 'LOC':[], 'GEO':[], 'GPE':[]})
+
+            i = 0
+            batch_text_raw = df_chunk['sent'].values
+            batch_text = [str(item) for item in batch_text_raw]
+            for doc in tqdm(nlp.pipe(batch_text, disable=["tok2vec", "tagger", "parser", "attribute_ruler", "lemmatizer"]), total=len(doc_ids)):
+                try:
+                    for ent in doc.ents:
+                        text = str(ent.text)
+                        if ((ent.label_ in NAMED_ENTITY_SET) and (text not in json_result['data'][i][ent.label_])):
+                            list_json_result[i][ent.label_].append(text)
+                except Exception as e:
+                    with open(f'result_alligned/errors_{file_split}.txt', 'a') as f:
+                        print(filename, str(e))
+                        f.write(str(e) + '\n')
+                i+=1
+
+            with open('result_alligned_NER/NER_{file_split}.json', mode='a+', encoding='utf-8') as f:
+                json.dump(list_json_result, f)
+
+            del df_chunk
+            gc.collect()
+
+        except Exception as e:
+            with open(f'result_alligned/errors_{file_split}.txt', 'a') as f:
+                print(filename, str(e))
+                f.write(str(e) + '\n')    
